8a73f013dacb280b455a44f3f8e564be429ab9ca
take0420 <zakisen81@gmail.com>
2024-04-14 22:45:40 +0900

    fix

--- BasicSyntax/if.py
+++ BasicSyntax/if.py
@@ -1,7 +1,7 @@
 n = 5
-# nが5より大きい場合、「positive」
-# nが0未満の場合、「negative」
-# nが0の場合、「zero」
+# nが5より大きい場合、positive
+# nが0未満の場合、negative
+# nが0の場合、zero
 if n > 0:
     print ("n is positive")
 elif n < 0:
