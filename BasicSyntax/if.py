8cebf83cfa44309106e3c227f09d7267dc6091b9
take0420 <zakisen81@gmail.com>
2024-06-07 09:40:46 +0900

    variable and if

--- BasicSyntax/if.py
+++ BasicSyntax/if.py
@@ -1,10 +1,33 @@
-n = 5
-# nが5より大きい場合、「positive」
-# nが0未満の場合、「negative」
-# nが0の場合、「zero」
+# nが正の数の場合、positive
+# nが負の数の場合、negative
+# nが0の場合、    0
+
+n = -3
+
 if n > 0:
-    print ("n is positive")
+    print("n is positive")
 elif n < 0:
-    print ("n is negative")
+    print("n is negative")
 else:
-    print ("n is zero")
+    print("n is zero")
+
+# 'and' 演算子の使用
+# 'or' 演算子の使用
+# 'not' 演算子の使用
+# Pythonでは範囲を条件にする時に以下のようにできます
+
+x, y = 5, 10
+if x > 0 and y > 0:
+    print("Both n and m are positive.")
+
+x = -5
+if x < 0 and y > 0:
+    print("Either x is positive or y is negative.")
+
+if not x > 0:
+    print("x is not positive.")
+
+if 0 < x < 10:
+    print("n is between 0 and 10")
+
+
--- BasicSyntax/variable.py
+++ BasicSyntax/variable.py
@@ -1,30 +1,11 @@
-# 変数の宣言と再代入
 n = 0
-print(n) # Output: 0
+n -= 1
+print(n)
 
-n = "ABC"
-print(n) # Output: ABC
+x = "ABC"
+print(x)
 
-# 複数変数への1行での代入
-a, b = 1, 2
-print(a, b) # Output: 1, 2
-
-# 変数のスワップ
 a, b = 1, 2
 a, b = b, a
-print(a, b) # Output: 2, 1
+print(a, b)
 
-# 複数変数への一括代入
-a = b = c = 0
-print(a, b, c) # Output: 0 0 0
-
-# Increment, Decrement
-n = 0
-n += 1
-print(n) # Output: 1
-n -= 1
-print(n) # Output: 0
-
-# None (NULL)
-n = None
-print(n) # Output: None
