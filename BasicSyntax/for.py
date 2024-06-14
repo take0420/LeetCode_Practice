e864dd2220a459e1a9929e17667a3b643773b5b9
take0420 <zakisen81@gmail.com>
2024-06-14 10:01:11 +0900

    feat

--- BasicSyntax/for.py
+++ BasicSyntax/for.py
@@ -1,3 +1,15 @@
 # 0から5までの数を順に出力
 for i in range (6):
     print(i)
+
+# 2から4までの数を順に出力します。
+for i in range (2, 5):
+    print(i)
+
+# 5から2までの数を逆順に出力します。
+for i in range (5, 1, -1):
+    print(i)
+
+# 4から0までの数を逆順に出力します。
+for i in reversed(range(5)):
+    print (i)
