fa71d0d942ca2babfd25adaeb6f6609cf1e2d687
take0420 <zakisen81@gmail.com>
2025-03-08 23:38:52 +0900

    Array & String

--- Prefix_Sum/running_sum_of_1d_array.py
+++ Prefix_Sum/running_sum_of_1d_array.py
@@ -8,3 +8,10 @@ class Solution:
             running_sum.append(total) # 更新した合計をリストに追加する
 
         return running_sum
+
+# Solution 2
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        for i in range(1, len(nums)):
+            nums[i] += nums[i - 1] # 累積和を直接更新する
+        return nums
